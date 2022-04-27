15bf85185b0abbcb4a8a014ad6d5fddfa5354791
dell <xyz@gmail.com>
2022-04-27 14:18:40 -0700

    Complete

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -12,7 +12,7 @@ const News = (props) => {
     // Function to Fetch First 8 articles from API
     const updateNews = async () => {
         let { country, category } = props;
-        const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=01da2a93a2634f2093f493581a18ded6&page=1&pageSize=8`;
+        const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=aefcd8d8971e45d39b955c0e5736d7d4&page=1&pageSize=8`;
         let data = await fetch(url);
         let parsedData = await data.json()
         setArticles(parsedData.articles)
@@ -25,7 +25,7 @@ const News = (props) => {
     // Fetch next 8 Articles.
     const fetchArticles = async () => {
         let { country, category } = props;
-        const data = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=01da2a93a2634f2093f493581a18ded6&page=${page}&pageSize=8`)
+        const data = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=aefcd8d8971e45d39b955c0e5736d7d4&page=${page}&pageSize=8`)
         let parsedData = await data.json()
         return parsedData.articles
     }
@@ -55,7 +55,7 @@ const News = (props) => {
                     {/* Get data of each article from api and send required data as props to the NewsItem component  */}
                     {articles.map((article) => {
                         return <div className='col-12 col-md-6 col-lg-4' key={article.url}>
-                            <NewsItem title={article.title ? article.title.slice(0, 63) + ".." : "No title Available"} Img={article.urlToImage} read_more={article.url} date={article.publishedAt} source={article.source.name} />
+                            <NewsItem title={article.title ? article.title.slice(0, 60) + ".." : "No title Available"} Img={article.urlToImage} read_more={article.url} date={article.publishedAt} source={article.source.name} />
                         </div>
                     })}
                 </div>

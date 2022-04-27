75e55c9b354dee8de3b760eb7419d0e1daebda93
shubham-allprojects <parab16aug@gmail.com>
2022-04-27 16:02:35 +0530

    ok

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,68 +1,83 @@
-import React, { useEffect, useState } from 'react'
-import NewsItem from './NewsItem'
-import InfiniteScroll from 'react-infinite-scroll-component'
-import Spinner from './Spinner'
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Spinner from "./Spinner";
 
 const News = (props) => {
-    // useState Hook for defining state for the component
-    const [articles, setArticles] = useState([])   // news articles in the API
-    const [page, setPage] = useState(2)
-    const [noMore, setNoMore] = useState(true)    // For infinite scroll Bar
+  // useState Hook for defining state for the component
+  const [articles, setArticles] = useState([]); // news articles in the API
+  const [page, setPage] = useState(2);
+  const [noMore, setNoMore] = useState(true); // For infinite scroll Bar
 
-    // Function to Fetch First 10 articles from API
-    const updateNews = async () => {
-        let { country, category } = props;
-        const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=be8a5dd01a42415b98abdbfa2e693f29&page=1&pageSize=10`;
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        setArticles(parsedData.articles)
-    }
-    useEffect(() => {
-        updateNews()
-        // eslint-disable-next-line 
-    }, [])
+  // Function to Fetch First 10 articles from API
+  const updateNews = async () => {
+    let { country, category } = props;
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=be8a5dd01a42415b98abdbfa2e693f29&page=1&pageSize=10`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(parsedData.articles);
+  };
+  useEffect(() => {
+    updateNews();
+    // eslint-disable-next-line
+  }, []);
 
-    // Fetch next 10 Articles.
-    const fetchArticles = async () => {
-        let { country, category } = props;
-        const data = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=be8a5dd01a42415b98abdbfa2e693f29&page=${page}&pageSize=10`)
-        let parsedData = await data.json()
-        return parsedData.articles
-    }
+  // Fetch next 10 Articles.
+  const fetchArticles = async () => {
+    let { country, category } = props;
+    const data = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=be8a5dd01a42415b98abdbfa2e693f29&page=${page}&pageSize=10`
+    );
+    let parsedData = await data.json();
+    return parsedData.articles;
+  };
 
-    // Function for Infinite Scroll.
-    const fetchMore = async () => {
-        const articlesFromApi = await fetchArticles();
-        setTimeout(() => {
-            setArticles([...articles, ...articlesFromApi]);
-        }, 1000);
-        if (articlesFromApi.length === 0 || articlesFromApi.length < 10) {
-            setNoMore(false)
-        }
-        setPage(page + 1)
+  // Function for Infinite Scroll.
+  const fetchMore = async () => {
+    const articlesFromApi = await fetchArticles();
+    setTimeout(() => {
+      setArticles([...articles, ...articlesFromApi]);
+    }, 1000);
+    if (articlesFromApi.length === 0 || articlesFromApi.length < 10) {
+      setNoMore(false);
     }
-    return (<>
-        {/* Infinite Scroll Bar */}
-        <InfiniteScroll
-            dataLength={articles.length}
-            next={fetchMore}
-            hasMore={noMore}
-            // Loader/Spinner image will be displayed e.g. Loading....  check Spinner Component.
-            loader={<Spinner />}
-        >
-            <div className='container'>
-                <div className='row d-flex justify-content-start'>
-                    {/* Get data of each article from api and send required data as props to the NewsItem component  */}
-                    {articles.map((article) => {
-                        return <div className='col-12 col-md-6 col-lg-4' key={article.url}>
-                            <NewsItem title={article.title ? article.title.slice(0, 64) + ".." : "No title Available"} Img={article.urlToImage} read_more={article.url} date={article.publishedAt} source={article.source.name} />
-                        </div>
-                    })}
+    setPage(page + 1);
+  };
+  return (
+    <>
+      {/* Infinite Scroll Bar */}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMore}
+        hasMore={noMore}
+        // Loader/Spinner image will be displayed e.g. Loading....  check Spinner Component.
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row d-flex justify-content-start">
+            {/* Get data of each article from api and send required data as props to the NewsItem component  */}
+            {articles.map((article) => {
+              return (
+                <div className="col-12 col-md-6 col-lg-4" key={article.url}>
+                  <NewsItem
+                    title={
+                      article.title
+                        ? article.title.slice(0, 64) + ".."
+                        : "No title Available"
+                    }
+                    Img={article.urlToImage}
+                    read_more={article.url}
+                    date={article.publishedAt}
+                    source={article.source.name}
+                  />
                 </div>
-            </div>
-        </InfiniteScroll>
+              );
+            })}
+          </div>
+        </div>
+      </InfiniteScroll>
     </>
-    )
-}
+  );
+};
 
-export default News
+export default News;
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import loading from './DefaultImages/loading.gif'
+import React from "react";
+import loading from "./DefaultImages/loading.gif";
 
 // Component for "lOading" image icon on the page while fetching next 10 articles from api.
 const Spinner = () => {
   return (
-    <div className='container d-flex justify-content-center'> <img className='spinner' src={loading} alt=".." /></div>
-  )
-}
+    <div className="container d-flex justify-content-center">
+      {" "}
+      <img className="spinner" src={loading} alt=".." />
+    </div>
+  );
+};
 
-export default Spinner
+export default Spinner;
